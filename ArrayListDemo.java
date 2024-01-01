3255c0b5b720218b00e8e220b1b4e51bee6afa23
BhomaRam <bhoma07062001@gmail.com>
2024-01-01 23:03:10 +0530

    Commit

---
+++ ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class ArrayListDemo
+{
+ public static void main(String...arg)
+ {
+  List<Integer> arr=new ArrayList<>();
+
+  arr.add(1);
+  arr.add(2);
+  arr.add(3);
+
+  System.out.println(arr);
+ }
+}
